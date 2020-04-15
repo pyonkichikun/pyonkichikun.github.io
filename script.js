54e1058df150f807077b62d77dfcfab886c51cc8
pyonkichikun <58249718+pyonkichikun@users.noreply.github.com>
2020-04-15 15:50:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(function(){
+$(function() {
+  var $c = $('#c');
 
-  $('.li').click(function() {
-    const $c = $('#c');
+  $('li').click(function() {
     if ($c.hasClass('close')) {
       $c.removeClass('close').slideDown(1000);
     } else {
@@ -18,27 +18,26 @@ $(function(){
   });
 
   $('#d-1-1').click(function() {
-    const $c = $('#c');
-    const $c1 = $('#c-1-1');
+    var $c11 = $('#c-1-1');
+
     if ($c.hasClass('close')) {
       $c.removeClass('close').slideDown(1000);
-    } else if ($c1.hasClass('close')) {
-      $c1.removeClass('close').fadeIn(500);
+    } else if ($c11.hasClass('close')) {
+      $c11.removeClass('close').fadeIn(500);
     } else {
-      $c1.addClass('close').fadeOut(500);
+      $c11.addClass('close').fadeOut(500);
     }
   });
 
   $('#d-1-2').click(function() {
-    const $c = $('#c');
-    const $c2 = $('#c-1-2');
+    var $c12 = $('#c-1-2');
+
     if ($c.hasClass('close')) {
       $c.removeClass('close').slideDown(1000);
-    } else if ($c2.hasClass('close')) {
-      $c2.removeClass('close').slideDown(500);
+    } else if ($c12.hasClass('close')) {
+      $c12.removeClass('close').slideDown(500);
     } else {
-      $c2.addClass('close').slideUp(500);
+      $c12.addClass('close').slideUp(500);
     }
   });
-
 });
